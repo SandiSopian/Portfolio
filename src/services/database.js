0f0a9c8dcb44c8e7690210d7598c7ce03606caa3
Sandi Sopian <shunasand73@gmail.com>
2025-01-29 15:56:00 +0700

    add some database

---
+++ src/services/database.js
@@ -0,0 +1,63 @@
+import ProjectImg1 from "../assets/images/project1.jpg";
+import ProjectImg2 from "../assets/images/project2.jpg";
+import ProjectImg3 from "../assets/images/project3.jpg";
+import ProjectImg4 from "../assets/images/project4.jpg";
+import ProjectImg5 from "../assets/images/project5.jpg";
+import ProfileImg from "../assets/images/profile-picture.jpg";
+
+// Social Media data
+export const socialMedia = [
+  {
+    socialMediaName: "Email",
+    socialMediaLink: "sandis17@outlook.com",
+  },
+  {
+    socialMediaName: "linkedin",
+    socialMediaLink: "https://www.linkedin.com/in/sandis17/",
+  },
+  {
+    socialMediaName: "Instagram",
+    socialMediaLink: "https://www.instagram.com/sarendia__/",
+  },
+];
+
+// About data
+export const about = {
+  profileImg: ProfileImg,
+  profileDescription: "I'm a creative developer based in Indonesia. I have a passion for web development and love to create new things. I'm currently working as a freelance developer and looking for new opportunities.",
+};
+
+// Projects data
+export const project = [
+  {
+    images: ProjectImg1,
+    projectTitle: "UI & UX",
+    projectDescription: "Designing a user interface and user experience for a website or app.",
+  },
+  {
+    images: ProjectImg2,
+    projectTitle: "Web & Mobile App",
+    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+  },
+  {
+    images: ProjectImg3,
+    projectTitle: "Web & Mobile App",
+    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+  },
+  {
+    images: ProjectImg4,
+    projectTitle: "Web & Mobile App",
+    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+  },
+  {
+    images: ProjectImg5,
+    projectTitle: "Web & Mobile App",
+    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+  },
+];
+
+// Contact data
+export const contact = {
+  email: "sandis17@outlook.com",
+  phone: "+6281288825373",
+};
