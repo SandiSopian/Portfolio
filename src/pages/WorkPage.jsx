ebf0932892f3467fb54618ef3b1d8045e3dcb28b
Sandi Sopian <shunasand73@gmail.com>
2025-02-10 11:09:54 +0700

    fix link on workpage

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -4,20 +4,26 @@ import { project } from "../services/database";
 const WorkPage = () => {
   return (
     <MainLayout>
-      <div className="flex flex-col md:flex-row h-svh text-white px-4 ml-24 md:ml-36 mt-6 md:mt-16 gap-3">
+      <div className="flex flex-col lg:flex-row h-svh text-white px-4 ml-24 md:ml-36 mt-6 md:mt-16 gap-3">
         <div className="md:basis-1/6">
           <h1 className="text-4xl md:text-8xl">Work</h1>
           <h2>{"-- Selected projects"}</h2>
         </div>
 
-        <div className="flex flex-col md:grid md:grid-cols-2 md:mt-48 md:basis-5/6">
+        <div className="flex flex-col md:grid md:grid-cols-2 md:gap-4 lg:mt-48 lg:ml-2 md:basis-5/6">
           {project.map((project, index) => (
             <div key={index} className={`relative ${index % 2 !== 0 ? "row-span-1 md:-mt-32" : "md:mt-0 md:mb-8"}`}>
-              <h3 className="mb-2 hover:text-gray-300 hover:underline cursor-pointer">{project.projectTitle}</h3>
+              <h3 className="mb-2 hover:text-gray-300 hover:underline cursor-pointer">
+                <a href={project.projectLink} target="_blank">
+                  {project.projectTitle}
+                </a>
+              </h3>
 
               <div className="max-w-md mb-6">
                 <div className="relative overflow-hidden">
-                  <img src={project.projectImage} alt={project.projectTitle} className="w-full h-auto object-cover cursor-pointer transition duration-500 hover:scale-110" />
+                  <a href={project.projectLink} target="_blank">
+                    <img src={project.projectImage} alt={project.projectTitle} className="w-full h-100 cursor-pointer transition duration-500 hover:scale-110" />
+                  </a>
                 </div>
               </div>
             </div>
--- src/services/database.js
+++ src/services/database.js
@@ -2,7 +2,7 @@ import ProjectImg1 from "../assets/images/project1.jpg";
 import ProjectImg2 from "../assets/images/project2.jpg";
 import ProjectImg3 from "../assets/images/project3.jpg";
 import ProjectImg4 from "../assets/images/project4.jpg";
-import ProjectImg5 from "../assets/images/project5.jpg";
+import ProjectImg5 from "../assets/images/project5.png";
 import ProfileImg from "../assets/images/profile-picture.jpg";
 import ContactImg from "../assets/images/contact-vector.png";
 
@@ -27,7 +27,7 @@ export const about = {
   profileImg: ProfileImg,
 
   profileDescription:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus rhoncus ante, in placerat odio iaculis in. Integer vulputate mi sit amet tellus facilisis, at varius tortor egestas. Mauris cursus augue ex, eget euismod felis blandit sit amet. Vivamus suscipit dolor at enim placerat porta. Aliquam elit lectus, accumsan vitae molestie in, fermentum quis dui. Nulla molestie enim vitae metus molestie bibendum. Aliquam sapien dolor, euismod eu eros non, porttitor tempor lorem. Suspendisse congue, massa vel faucibus luctus, nulla urna feugiat erat, vel efficitur dolor elit vel justo. Curabitur condimentum, orci nec accumsan dapibus, ipsum libero aliquet velit, sed lacinia sem purus at mi. Nunc nibh odio, ullamcorper vel consequat in, eleifend sed dolor. Morbi iaculis odio a lacinia consectetur. Praesent quam nibh, facilisis eget urna a, dictum efficitur libero. Maecenas posuere quis risus at posuere. Nulla facilisis efficitur enim in tincidunt. Nullam ac venenatis est, at fermentum est. Vestibulum volutpat interdum mattis. Pellentesque ante ipsum, faucibus id dolor vitae, rhoncus rhoncus erat. Suspendisse euismod condimentum sapien, egestas gravida leo iaculis et. Sed vestibulum bibendum purus, tempor tincidunt risus ultricies at. Morbi lobortis justo justo, id malesuada sem commodo ut. Aenean blandit felis nulla, non venenatis libero venenatis vel. Vestibulum scelerisque nibh laoreet arcu venenatis vehicula. Praesent interdum rutrum justo, mattis bibendum nulla dignissim ac. Nulla semper nulla quis metus tristique varius. Vestibulum auctor nisl a nibh sodales, vel tincidunt velit porta. Ut elit urna, finibus vitae cursus eu, ultricies at turpis. Quisque nec maximus nisi, eget faucibus orci. Vestibulum sed mattis augue, vel cursus magna. Donec faucibus sit amet purus sit amet pharetra. Sed in magna pharetra, volutpat sem in, fringilla odio. Vestibulum quis enim ex. Nam varius fringilla placerat. In hac habitasse platea dictumst. Suspendisse fringilla accumsan enim, et mattis arcu venenatis quis. Nunc pellentesque, ipsum eget dictum finibus, tellus dui cursus ipsum, id sollicitudin urna ligula eu purus. Vivamus et bibendum est, sed maximus leo. Quisque quis mollis eros. Phasellus dictum dolor id metus varius vehicula. Suspendisse potenti. Fusce laoreet volutpat sagittis. Suspendisse venenatis aliquam efficitur. Phasellus sollicitudin sem ac justo volutpat vehicula. Aliquam ultricies aliquet massa id laoreet. Etiam accumsan elit consectetur, efficitur libero nec, placerat neque. Fusce imperdiet libero sed viverra aliquet. Nunc sit amet volutpat risus. Nulla vitae luctus enim. Curabitur sit amet leo facilisis, convallis mauris molestie, vulputate dui. Donec et bibendum arcu, eget feugiat ante. Nulla porta dui bibendum mi ultricies interdum. ",
+    "I graduated with a Bachelor of Computer Science from Bale Bandung University, my first work experience was as a sales clerk and double as a cashier at a mini market. I was also a printing laboratory operator working in the textile sector. Now I am a self-employer freelancer in the field of web development and content creator.",
 
   // profileDescription: "I'm a creative developer based in Indonesia. I have a passion for web development and love to create new things. I'm currently working as a freelance developer and looking for new opportunities.",
 };
@@ -36,43 +36,33 @@ export const about = {
 export const project = [
   {
     projectImage: ProjectImg1,
-    projectTitle: "UI & UX",
-    projectDescription: "Designing a user interface and user experience for a website or app.",
+    projectTitle: "Jongko Daging Mang Umis",
+    projectDescription: "I'm build simple Landing Pages for selling meats of cow and chickens",
+    projectLink: "https://jongko-daging-mang-umis.vercel.app/",
   },
   {
     projectImage: ProjectImg2,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+    projectTitle: "Venendria plants shop",
+    projectDescription: "I'm slicing my own figma design for e-commerce plants shop",
+    projectLink: "https://figma-to-next-js-tailwind-slicing-ver-1.vercel.app/",
   },
   {
     projectImage: ProjectImg3,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+    projectTitle: "Sarang Coffee cafe",
+    projectDescription: "I'm slicing my own figma design for bussiness landing page coffee shop",
+    projectLink: "https://figma-to-next-js-tailwind-slicing-sarang-coffee-cafe.vercel.app/",
   },
   {
     projectImage: ProjectImg4,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+    projectTitle: "Tara Trip Gallery",
+    projectDescription: "I'm slicing my own figma design for light gallery traveling website",
+    projectLink: "https://figma-to-next-js-tailwind-slicing-tara-trip-gallery.vercel.app/",
   },
   {
     projectImage: ProjectImg5,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
-  },
-  {
-    projectImage: ProjectImg5,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
-  },
-  {
-    projectImage: ProjectImg5,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
-  },
-  {
-    projectImage: ProjectImg5,
-    projectTitle: "Web & Mobile App",
-    projectDescription: "Brainstroming ideas into exeptional web and mobile app.",
+    projectTitle: "Clean and Simple Portfolio",
+    projectDescription: "I'm create clean and simple portfolio with static page",
+    projectLink: "https://simple-portfolio-uiux.vercel.app/",
   },
 ];
 
